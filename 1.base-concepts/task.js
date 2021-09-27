55983afb4fe5415067ee8916b808d9d0e4b2cf86
Polina Titova <titova_polina@mail.ru>
2021-09-28 00:59:56 +0300

    Task 2 is done!

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -19,10 +19,44 @@ function solveEquation(a, b, c) {
   return arr; // array
 }
 
+function monthDiff(d1, d2) {
+  let months;
+  months = (d2.getFullYear() - d1.getFullYear()) * 12;
+  months -= d1.getMonth();
+  months += d2.getMonth();
+
+  if(months < 0) {
+    months = 0;
+  }
+
+  return months;
+}
+
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
 
-  // код для задачи №2 писать здесь
+  if(Number.isNaN(Number(percent))) {
+    return `Параметр \"Процентная ставка\" содержит неправильное значение \"${percent}\"`;
+  }
+
+  if(Number.isNaN(Number(contribution))) {
+    return `Параметр \"Начальный взнос\" содержит неправильное значение \"${contribution}\"`;
+  }
+
+  if(Number.isNaN(Number(amount))) {
+    return `Параметр \"Общая стоимость\" содержит неправильное значение \"${amount}\"`;
+  }
+
+  let S = amount - contribution;
+  let P = (percent / 100) / 12;
+  let currentDate = new Date();
+  let monthsCount = monthDiff(currentDate, date);
+
+  let payment = S * (P + P / (((1 + P)**monthsCount) - 1));
+
+  totalAmount = Number((monthsCount * payment).toFixed(2));
+
+  console.log(totalAmount);
 
   return totalAmount;
 }
