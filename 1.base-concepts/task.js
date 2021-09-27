d06fb2c9e71d266abe81ba99b8834fe7d8bb32d9
Polina Titova <titova_polina@mail.ru>
2021-09-27 23:43:55 +0300

    Task 1 is done

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,6 +1,21 @@
+"use strict";
+
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
+  let arr = [];
+  let D = b**2 - 4 * a * c;
+
+  if (D < 0) {
+    arr = [];
+  } else if (D === 0) {
+    let x = -b / (2 * a);
+    arr.push(x);
+  } else if (D > 0) {
+    let x1 = (-b + Math.sqrt(D)) / (2 * a);
+    let x2 = (-b - Math.sqrt(D)) / (2 * a);
+    arr.push(x1);
+    arr.push(x2);
+  }
+  
   return arr; // array
 }
 
