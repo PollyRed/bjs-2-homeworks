3ab33de7541d074bd965e15c8daf2aa5a1fbcbaf
Polina Titova <titova_polina@mail.ru>
2021-11-02 02:00:24 +0300

    Homework 8 is done

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,12 +1,67 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+  let cache = [];
+
+  function wrapper(...args) {
+      const hash = args.join(',');
+      let idx = cache.findIndex((item)=> item.hash === hash); 
+      if (idx !== -1 ) {
+          console.log("Из кэша: " + cache[idx].value); 
+          return "Из кэша: " + cache[idx].value;
+      } else {
+          let result = func(...args); 
+          cache.push({hash: hash, value: result}) ; 
+          if (cache.length > 5) { 
+            cache.shift();
+          }
+          console.log("Вычисляем: " + result);
+          return "Вычисляем: " + result;  
+      }
+  }
+
+  return wrapper;
 }
 
 
-function debounceDecoratorNew(func) {
-  // Ваш код
+function debounceDecoratorNew(func, ms) {
+  let timeout;
+  let isDelayed = false;
+
+  function wrapper(...args) {        
+    clearTimeout(timeout);
+    
+    if(isDelayed) {
+      timeout = setTimeout(() => {            
+        func.apply(this, args);            
+        console.timeEnd("time");   
+      }, ms);  
+    } else {
+      isDelayed = true;
+      func.apply(this, args);
+    }     
+  };
+
+  return wrapper;
 }
 
-function debounceDecorator2(func) {
-  // Ваш код
+function debounceDecorator2(func, ms) {
+  let timeout;
+  let isDelayed = false;
+
+  function wrapper(...args) {
+    ++wrapper.count;
+    clearTimeout(timeout);
+    
+    if(isDelayed) {
+      timeout = setTimeout(() => {            
+        func.apply(this, args);            
+        console.timeEnd("time");   
+      }, ms);  
+    } else {
+      isDelayed = true;
+      func.apply(this, args);
+    }     
+  };
+
+  wrapper.count = 0;
+  return wrapper;
 }
